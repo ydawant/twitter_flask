527841ca9785b4984226fc637de9f5ad3e341251
Yannick Dawant <yannick21@gmail.com>
2015-05-10 12:35:36 -0500

    updated ajax URLs

--- static/tweets.js
+++ static/tweets.js
@@ -2,12 +2,12 @@ angular.module('tweetsApp', [])
 	.controller('TweetListCtrl', ['$scope', '$http', '$filter', function($scope, $http, $filter) {
 	    $scope.submit = function() {
 	      if ($scope.handle) {
-            $http.get('https://twitter-flask.herokuapp.com/' + $scope.handle + "/").success(function(data) {      
+            $http.get('https://twitter-flask.herokuapp.com/tweets/' + $scope.handle + "/").success(function(data) {      
             $scope.tweets = data['tweets'];
   				});
         }
         if ($scope.handle) {
-            $http.get('https://twitter-flask.herokuapp.com/' + $scope.handle + "/").success(function(data) {
+            $http.get('https://twitter-flask.herokuapp.com/user/' + $scope.handle + "/").success(function(data) {
   					$scope.user = data['user'];
         	});
         }
