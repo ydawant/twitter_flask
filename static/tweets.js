6f22afc021759e929b243c6ff7be5c649ed21a96
Yannick Dawant <yannick21@gmail.com>
2015-05-10 12:24:59 -0500

    updated for heroku

--- static/tweets.js
+++ static/tweets.js
@@ -2,12 +2,12 @@ angular.module('tweetsApp', [])
 	.controller('TweetListCtrl', ['$scope', '$http', '$filter', function($scope, $http, $filter) {
 	    $scope.submit = function() {
 	      if ($scope.handle) {
-            $http.get('http://127.0.0.1:5000/tweets/' + $scope.handle + "/").success(function(data) {      
+            $http.get('https://twitter-flask.herokuapp.com/' + $scope.handle + "/").success(function(data) {      
             $scope.tweets = data['tweets'];
   				});
         }
         if ($scope.handle) {
-            $http.get('http://127.0.0.1:5000/user/' + $scope.handle + "/").success(function(data) {
+            $http.get('https://twitter-flask.herokuapp.com/' + $scope.handle + "/").success(function(data) {
   					$scope.user = data['user'];
         	});
         }
