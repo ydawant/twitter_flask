d47b9732431cee5577cbf08b0aa6f2d27b7c7f77
Yannick Dawant <yannick21@gmail.com>
2015-05-10 11:53:17 -0500

    working filter for photos

--- static/tweets.js
+++ static/tweets.js
@@ -1,15 +1,24 @@
 angular.module('tweetsApp', [])
-	.controller('TweetListCtrl', ['$scope', '$http', function($scope, $http) {
+	.controller('TweetListCtrl', ['$scope', '$http', '$filter', function($scope, $http, $filter) {
 	    $scope.submit = function() {
 	      if ($scope.handle) {
-            $http.get('http://127.0.0.1:5000/tweets/' + $scope.handle + "/").success(function(data) {
-  					$scope.tweets = data['tweets'];      
+            $http.get('http://127.0.0.1:5000/tweets/' + $scope.handle + "/").success(function(data) {      
+            $scope.tweets = data['tweets'];
   				});
         }
         if ($scope.handle) {
             $http.get('http://127.0.0.1:5000/user/' + $scope.handle + "/").success(function(data) {
   					$scope.user = data['user'];
         	});
+        }
+
+        $scope.picFilter = function(tweets) {
+          if ($scope.search == 'picture') {
+            return $filter('filter')($scope.tweets, {picture:'!null'});
+          }
+          else {
+            return $scope.tweets
+          }
         }
   		}
 		}]);
